1e25ea4f461c761726b7316f9a78bed88612d360
brandon <brandon@ijun-uui-Macmini.local>
2022-02-22 07:50:36 +0900

    feat : EurekaServer 적용

--- Catalogs/src/main/java/com/msa/catalogs/CatalogsApplication.java
+++ Catalogs/src/main/java/com/msa/catalogs/CatalogsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 //@ComponentScan("com.*")
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableEurekaClient
 @SpringBootApplication
 public class CatalogsApplication {
 
